bd175fff136ee4ffebd04b6f4ebebdedd95cbced
Sven-27 <sven.notermans@live.nl>
2021-08-08 21:45:04 +0200

    deleted backend

--- tracker-app-backend/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '12d3ebb736327c90e3c1d66e27c52fb7'),
-    },
-  },
-});
--- tracker-app-backend/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '0c7c66f2-728f-4aaf-89a0-ba43daa7a94c'
-};
