ea168fd65ef6e457e0705027c6fcb4416b21854f
Sven-27 <sven.notermans@live.nl>
2021-08-15 15:21:50 +0200

    created a form in login

--- src/components/webPages/login/Login.tsx
+++ src/components/webPages/login/Login.tsx
@@ -3,23 +3,33 @@ import React from 'react'
 const Login = () => {
   return (
     <div className="grid place-items-center h-screen">
-    <div className="flex justify-center flex-col items-center bg-white p-10 rounded-lg shadow-md ">
-    {/* <h1 className="text-xl font-bold">Jump back in!</h1>
-    <div className="mt-4 mb-10">
-      <p className="text-gray-600">Course 75% completed</p>
-      <div className="bg-gray-400 w-64 h-3 rounded-lg mt-2 overflow-hidden">
-        <div className="bg-pink-400 w-3/4 h-full rounded-lg shadow-md"></div>
+      <div className="flex justify-center flex-col items-center bg-white p-10 rounded-lg shadow-md ">
+        <form className="block">
+          <legend>Sign Up</legend>
+          <section className="flex flex-col">
+            <label htmlFor="firstname">Firstname:</label>
+            <input type="text" name="firstname" id="firstname" className="form-input px-1 py-1 rounded border" placeholder="John" />
+          </section>
+          <section className="flex flex-col">
+            <label htmlFor="lastname">Lastname:</label>
+            <input type="text" name="lastname" id="lastname" className="form-input px-1 py-1 rounded border" placeholder="Doe" />
+          </section>
+          <section className="flex flex-col">
+            <label htmlFor="email">Email:</label>
+            <input type="email" name="email" id="email" className="form-input px-1 py-1 rounded border" placeholder="john.doe@example.com" />
+          </section>
+          <section className="flex flex-col">
+            <label htmlFor="password">Password:</label>
+            <input type="password" name="password" id="password" className="form-input px-1 py-1 rounded border" />
+          </section>
+          <section className="flex flex-col">
+            <label htmlFor="password2">Confirm Password:</label>
+            <input type="password" name="password2" id="password2" className="form-input px-1 py-1 rounded border" />
+          </section>
+          
+        </form>
       </div>
     </div>
-    <h3 className="text-xs uppercase">Current lesson:</h3>
-    <h2 className="tracking-wide">
-      Object in JavaScript
-      <br />
-      (Challenge)
-    </h2>
-    <button className="bg-orange-400 py-3 px-8 mt-4 rounded text-sm font-semibold hover:bg-opacity-75">Go to lesson</button> */}
-  </div>
-  </div>
   )
 }
 
