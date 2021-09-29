543af95d793a9bafbf2f695905ec6e576d03eadf
Sven-27 <sven.notermans@live.nl>
2021-09-29 18:39:28 +0200

    worked on theme

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,6 @@
-// If you want to use other PostCSS plugins, see the following:
-// https://tailwindcss.com/docs/using-with-preprocessors
-const tailwindcss = require('tailwindcss');
-
 module.exports = {
-  plugins: [
-    require('postcss-import'),
-    tailwindcss('./tailwind.config.js'),
-    require('autoprefixer'),
-  ],
-};
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
-import 'tailwindcss/tailwind.css'
+import { ThemeProvider } from 'next-themes'
 import type { AppProps } from 'next/app' 
 import 'styles/globals.css'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-
-   return <Component {...pageProps} />
-
+  return(
+  <ThemeProvider>
+    <Component {...pageProps} />
+  </ThemeProvider>
+  )
 }
 
 export default MyApp
