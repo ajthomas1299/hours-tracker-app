0fe71c2ff86fa67c38d39d557ff7dcaf04c82ef0
Sven-27 <sven.notermans@live.nl>
2021-11-05 18:43:09 +0100

    adjusting textarea

--- src/components/webPages/hoursTracker/EnterDescription.tsx
+++ src/components/webPages/hoursTracker/EnterDescription.tsx
@@ -3,7 +3,7 @@ const EnterDescription = () => {
     return (
         <div className="w-screen flex flex-col items-center mb-6 sml:mb-3 sml:mt-2">
          
-            <textarea  placeholder="Enter Description..." name="description">
+            <textarea placeholder="Enter Description..." name="description">
             </textarea>
         </div>
     )
